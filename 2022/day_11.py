fb7772180180a1efda212888aa825a119118d0e0
Sandro Mund <sandro.mund@consistec.de>
2023-02-09 10:07:01 +0100

    day_11_task_1

--- 2022/day_11.py
+++ 2022/day_11.py
@@ -9,13 +9,10 @@ class Monkey:
         self.if_false_throw_to = None
         self.operation_function = None
         self.function_str = None
+        self.inspected_items = 0
 
     def __str__(self):
         return f"id:{self.id} \nitems: {self.starting_items}\noperation: {self.operation} \ndivisible_by: {self.divisible_by} \n"
-
-    def operation(self, x):
-        args = self.operation
-        print()
 
 
 
@@ -55,27 +52,73 @@ def read_data(path):
         m.if_true_throw_to = monkeys[m.if_true_throw_to]
     return monkeys
 
-def turn(monkey_list):
+def turn(monkey_list, debug_print=False):
     for m in monkey_list:
-        print(f"Monkey: {m.id}")
+        if debug_print:
+            print(f"Monkey: {m.id}")
         for i in m.starting_items:
-            print(f"    Monkey inspects an item with a worry level of {i}.")
+            m.inspected_items += 1
+            if debug_print:
+                print(f"    Monkey inspects an item with a worry level of {i}.")
             x = m.starting_items[0]
             new_x = m.operation_function(x)
-            print("        Worry level is x="+str(x)+ " -> " + m.function_str + " = " + str(new_x))
+            if debug_print:
+                print("        Worry level is x="+str(x)+ " -> " + m.function_str + " = " + str(new_x))
             new_x = new_x // 3
-            print(f"        Monkey gets bored with item. Worry level is divided by 3 to {new_x}.")
+            if debug_print:
+                print(f"        Monkey gets bored with item. Worry level is divided by 3 to {new_x}.")
             if new_x % m.divisible_by != 0:
-                print(f"        Current worry level is NOT divisible by {m.divisible_by }.")
-                print(f"        Item with worry level {new_x} is thrown to monkey {m.if_false_throw_to.id}.")
+                if debug_print:
+                    print(f"        Current worry level is NOT divisible by {m.divisible_by }.")
+                    print(f"        Item with worry level {new_x} is thrown to monkey {m.if_false_throw_to.id}.")
                 m.if_false_throw_to.starting_items.append(new_x)
             else:
-                print(f"        Current worry level is divisible by {m.divisible_by }.")
-                print(f"        Item with worry level {new_x} is thrown to monkey {m.if_true_throw_to.id}.")
-                m.if_false_throw_to.starting_items.append(new_x)
+                if debug_print:
+                    print(f"        Current worry level is divisible by {m.divisible_by }.")
+                    print(f"        Item with worry level {new_x} is thrown to monkey {m.if_true_throw_to.id}.")
+                m.if_true_throw_to.starting_items.append(new_x)
             m.starting_items = m.starting_items[1:]
 
-if __name__ == '__main__':
-    monkeys = read_data(path="data/day11_example.txt")
+def rounds(n, monkey_list, debug_print=False):
+    for i in range(1, n+1):
+        turn(monkey_list, debug_print=debug_print)
+    if debug_print:
+        print(f"\nAfter round {i}, the monkeys are holding items with these worry levels:")
+        for m in monkey_list:
+            print(f"Monkey {m.id} : {m.starting_items}")
+        print()
 
-    turn(monkeys)
+def day_11_example(debug_print=False):
+    monkeys = read_data(path="data/day11_example.txt")
+    rounds(20, monkeys)
+    monkey_business = []
+    for m in monkeys:
+        monkey_business.append(m.inspected_items)
+        if debug_print:
+            print(f"Monkey {m.id} inspected items {m.inspected_items} times.")
+
+    monkey_business = sorted(monkey_business, reverse=True)[:2]
+    monkey_business_level = monkey_business[0] * monkey_business[1]
+    if debug_print:
+        print(f"\nMonkey business level: {monkey_business_level}")
+    return monkey_business_level
+
+def day_11_task_1(debug_print=False):
+    monkeys = read_data(path="data/day11.txt")
+    rounds(20, monkeys)
+    monkey_business = []
+    for m in monkeys:
+        monkey_business.append(m.inspected_items)
+        if debug_print:
+            print(f"Monkey {m.id} inspected items {m.inspected_items} times.")
+
+    monkey_business = sorted(monkey_business, reverse=True)[:2]
+    monkey_business_level = monkey_business[0] * monkey_business[1]
+    if debug_print:
+        print(f"\nMonkey business level: {monkey_business_level}")
+    return monkey_business_level
+
+
+if __name__ == '__main__':
+    assert day_11_example() == 10605
+    assert  day_11_task_1() == 56120
