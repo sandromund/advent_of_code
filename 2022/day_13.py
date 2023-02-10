f0119c40b9ae287030a9d944159aefc93e2e7cf4
Sandro Mund <sandro.mund@consistec.de>
2023-02-10 14:59:34 +0100

    Day 13 task 1 complete

--- 2022/day_13.py
+++ 2022/day_13.py
@@ -5,35 +5,44 @@ class Packet:
         self.right = None
         self.in_the_right_order = None
 
-    def compare(self, left, right):
+    def compare(self, left, right, depth=0):
         """
         """
+        if self.in_the_right_order is not None:
+            return
+        print(depth * "   " + f"- Compare {left} vs {right}")
+
         if type(left) == int == type(right):
             if left < right:
+                print((1 + depth) * "   " + "- Left side is smaller, so inputs are in the right order")
                 self.in_the_right_order = True
-            if right > left:
+            if left > right:
+                print((1 + depth) * "   " + "- Right side is smaller, so inputs are not in the right order")
                 self.in_the_right_order = False
 
         elif type(left) == list == type(right):
 
             if len(left) == len(right):
                 for l, r in zip(left, right):
-                    self.compare(l, r)
+                    self.compare(l, r, depth=depth + 1)
             else:
                 for i in range(min([len(left), len(right)])):
-                    self.compare(left[i], right[i])
+                    self.compare(left[i], right[i], depth=depth + 1)
                 if self.in_the_right_order is None:
                     if len(left) > len(right):
+                        print(depth * "   " + f"Right side ran out of items, so inputs are in the right order")
                         self.in_the_right_order = False
                     else:
+                        print(depth * "   " + f"Left side ran out of items, so inputs are in the right order")
                         self.in_the_right_order = True
         elif type(left) == list and type(right) == int:
-            self.compare(left, [right])
+            print(depth * "   " + f"- Mixed types; convert right to [{right}] and retry comparison")
+            self.compare(left, [right], depth=depth + 1)
         elif type(left) == int and type(right) == list:
-            self.compare([left], right)
+            print(depth * "   " + f"- Mixed types; convert left to [{left}] and retry comparison")
+            self.compare([left], right, depth=depth + 1)
         else:
             raise TypeError
-
 
 
 def read_data(path: str) -> list[Packet]:
@@ -61,10 +70,33 @@ def read_data(path: str) -> list[Packet]:
     return packet_list
 
 
-if __name__ == '__main__':
+def day_13_example():
     data: list[Packet] = read_data(path="data/day_13_example.txt")
 
     for p in data:
-        #print(p.left, ",", p.right)
         p.compare(left=p.left, right=p.right)
-        print("in_the_right_order -->" ,p.in_the_right_order)
+
+    sum_indices = 0
+    for i in range(len(data)):
+        if data[i].in_the_right_order:
+            sum_indices += i + 1
+    return sum_indices
+
+
+def day_13_task_1():
+    data: list[Packet] = read_data(path="data/day_13.txt")
+
+    for p in data:
+        p.compare(left=p.left, right=p.right)
+
+    sum_indices = 0
+    for i in range(len(data)):
+        if data[i].in_the_right_order:
+            sum_indices += i + 1
+    return sum_indices
+
+
+if __name__ == '__main__':
+    assert day_13_example() == 13
+    assert day_13_task_1() == 6235
+    print(day_13_task_1())
