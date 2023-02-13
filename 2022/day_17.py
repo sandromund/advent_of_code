80d43e69b2a76db0b1f93f3f9e60e67b52235838
Sandro Mund <sandro.mund@consistec.de>
2023-02-13 12:45:26 +0100

    WIP day 17

--- 2022/day_17.py
+++ 2022/day_17.py
@@ -59,7 +59,9 @@ class Chamber:
         for i in range(len(self.data) - 1, -1, -1):
             line_str = ""
             for number in self.data[i]:
-                if number > 0:
+                if (number, i) in self.current_rock_points:
+                    line_str += " @ "
+                elif number > 0:
                     line_str += " # "
                 else:
                     line_str += " . "
@@ -73,6 +75,15 @@ class Chamber:
          units above the highest rock in the room (or the floor, if there isn't one).
         :return:
         """
+
+        data = []
+        # re remove all rows with only zeros:
+        for i in range(len(self.data)):
+            if len([x for x in self.data[i] if x > 0]) == 0:
+                break
+            data.append(self.data[i])
+        self.data = data
+
         for _ in range(3):
             self.data.append([0] * self.wide)
         r = self.rocks[self.current_rock_index]
@@ -89,22 +100,36 @@ class Chamber:
         self.current_rock_index += 1
         if self.current_rock_index == len(self.rocks):
             self.current_rock_index = 0
-        self.data = np.array(self.data)
         self.current_rock_points = rock_positions
 
     def move_rock_down(self):
+        """
+        If a downward movement would cause a falling rock to move into the floor or an already-fallen rock,
+        the falling rock stops where it is (having landed on something) and a new rock immediately begins falling.
+        """
+        invalid_move = False
+
         if any(map(lambda e: e[1] - 1 < 0, self.current_rock_points)):
-            return
-        new_rock_points = set()
-        new_data = self.data
-        for x, y in self.current_rock_points:
-            if (x, y - 1) not in self.current_rock_points and new_data[y - 1][x] != 0:
-                return
-            new_rock_points.add((x, y - 1))
-            new_data[y][x] = 0
-            new_data[y - 1][x] = 1
-        self.current_rock_points = new_rock_points
-        self.data = np.array(new_data)
+            invalid_move = True
+
+        if not invalid_move:
+            new_rock_points = set()
+            new_data = self.data
+            for x, y in self.current_rock_points:
+                if (x, y - 1) not in self.current_rock_points and new_data[y - 1][x] != 0:
+                    invalid_move = True
+                    break
+                new_rock_points.add((x, y - 1))
+                new_data[y][x] = 0
+                new_data[y - 1][x] = 1
+
+            if not invalid_move:
+                self.current_rock_points = new_rock_points
+                self.data = new_data
+            else:
+                self.spawn_rock()
+        else:
+            self.spawn_rock()
 
     def move_rock_left(self):
         if any(map(lambda e: e[0] - 1 < 0, self.current_rock_points)):
@@ -119,11 +144,12 @@ class Chamber:
             new_data[y][x] = 0
             new_data[y][x - 1] = 1
         self.current_rock_points = new_rock_points
-        self.data = np.array(new_data)
+        self.data = new_data
 
     def move_rock_right(self):
-        if any(map(lambda e: e[0] + 1 >= self.wide, self.current_rock_points)):
-            return
+        for x, _ in self.current_rock_points:
+            if x + 1 >= self.wide:
+                return
         new_rock_points = set()
         new_data = self.data
         for x, y in sorted(list(self.current_rock_points), reverse=True):
@@ -134,7 +160,10 @@ class Chamber:
             new_data[y][x] = 0
             new_data[y][x + 1] = 1
         self.current_rock_points = new_rock_points
-        self.data = np.array(new_data)
+        self.data = new_data
+
+    def run(self):
+        pass
 
 
 def day_17_task_1_example():
@@ -142,12 +171,10 @@ def day_17_task_1_example():
     chamber.spawn_rock()
     chamber.print_chamber()
 
-    for _ in range(5):
+    for _ in range(20):
         chamber.move_rock_down()
-        chamber.move_rock_right()
         chamber.print_chamber()
-    chamber.move_rock_left()
-    chamber.print_chamber()
+
     print(chamber.data)
 
 
