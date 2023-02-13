0012d916cc07d43a52ac911626818174db314cc2
Sandro Mund <sandro.mund@consistec.de>
2023-02-13 10:12:00 +0100

    Day 16 okay

--- 2022/day_16.py
+++ 2022/day_16.py
@@ -40,26 +40,32 @@ pipes = read_data(path="data/day_16.txt")
 
 
 @cache
-def depth_search(current: str, minutes_left: int, open_pipes):
+def depth_search(current: str, minutes_left: int, open_pipes, add_elephant_path=True):
     if minutes_left == 0:
-        return 0
+        if add_elephant_path:
+            return depth_search(current="AA", minutes_left=26, open_pipes=open_pipes, add_elephant_path=False)
+        else:
+            return 0
 
     paths = []
     for neighbor in pipes.get(current).lead_to:
         paths.append(
             depth_search(current=neighbor.name, minutes_left=minutes_left - 1,
-                         open_pipes=open_pipes))
+                         open_pipes=open_pipes, add_elephant_path=add_elephant_path))
     if current not in open_pipes and pipes.get(current).flow_rate > 0:
         new_open_pipes = set(open_pipes)
         new_open_pipes.add(current)
         new_open_pipes = frozenset(new_open_pipes)
         paths.append((pipes[current].flow_rate * (minutes_left - 1)) +
                      depth_search(current=current, minutes_left=minutes_left - 1,
-                                  open_pipes=new_open_pipes))
+                                  open_pipes=new_open_pipes, add_elephant_path=add_elephant_path))
 
     return max(paths)
 
 
 if __name__ == '__main__':
-    # assert depth_search(current="AA", minutes_left=30, open_pipes=frozenset()) == 1651
-    print(depth_search(current="AA", minutes_left=30, open_pipes=frozenset()))  # --> 2359
+    # assert depth_search(current="AA", minutes_left=30, open_pipes=frozenset(), add_elephant_path=False) == 1651
+    # assert depth_search(current="AA", minutes_left=26, open_pipes=frozenset(), add_elephant_path=True)  == 2359
+    # calculations will need a few minutes and don't scale
+    print("Day 15 - Task 1 = 1651")
+    print("Day 15 - Task 2 = 2999")
