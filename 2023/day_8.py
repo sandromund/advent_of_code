b5dacff6c6646f2c6c01d2bfc3adbffa12d0e194
Sandro Mund <sandro.mund@consistec.de>
2023-12-08 15:34:22 +0100

    23 d8 t2

--- 2023/day_8.py
+++ 2023/day_8.py
@@ -1,7 +1,4 @@
-"""
---- Day 8: Haunted Wasteland ---
-
-"""
+import math
 
 
 def read_data(path):
@@ -35,7 +32,40 @@ def day_8_task_1(data):
     return n_steps
 
 
+def day_8_task_2_brute_force(data):
+    instructions, mapping = data
+    current_positions = [start for start in mapping.keys() if start.endswith("A")]
+    n = len(current_positions)
+    n_steps = 0
+    tuple_mapping = {"L": 0, "R": 1}
+    while n != len([p for p in current_positions if p.endswith("Z")]):
+        for direction in instructions:
+            for i in range(n):
+                c = current_positions[i]
+                current_positions[i] = mapping[current_positions[i]][tuple_mapping[direction]]
+                print(c, "->", current_positions[i])
+            n_steps += 1
+    return n_steps
+
+
+def day_8_task_2(data):
+    instructions, mapping = data
+    current_positions = [start for start in mapping.keys() if start.endswith("A")]
+    tuple_mapping = {"L": 0, "R": 1}
+    solutions = []
+    for current_position in current_positions:
+        n_steps = 0
+        while not current_position.endswith("Z"):
+            for direction in instructions:
+                current_position = mapping[current_position][tuple_mapping[direction]]
+                n_steps += 1
+        solutions.append(n_steps)
+    return math.lcm(*solutions)
+
+
 if __name__ == '__main__':
     assert day_8_task_1(read_data(path="data/day_8_demo_1.txt")) == 2
     assert day_8_task_1(read_data(path="data/day_8_demo_2.txt")) == 6
     assert day_8_task_1(read_data(path="data/day_8.txt")) == 15989
+    assert day_8_task_2(read_data(path="data/day_8_demo_3.txt")) == 6
+    assert day_8_task_2(read_data(path="data/day_8.txt")) == 13830919117339
