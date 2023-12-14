e53fa41fc04e87f2b297a3de3385365b1ad85103
Sandro Mund <sandro.mund@consistec.de>
2023-12-14 18:18:13 +0100

    Minor changes

--- 2023/day_14.py
+++ 2023/day_14.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 
 def read_data(path):
@@ -33,14 +34,73 @@ def day_14_task_1(data):
     return score(iterate(data))
 
 
+def cycle_north(data):
+    return iterate(data)
+
+
+def cycle_south(data):
+    something_changed = True
+    while something_changed:
+        something_changed = False
+        for row in range(len(data) - 2, -1, -1):
+            for col in range(len(data[0])):
+                if data[row][col] == "O" and data[row + 1][col] == ".":
+                    data[row][col], data[row + 1][col] = data[row + 1][col], data[row][col]
+                    something_changed = True
+    return data
+
+
+def cycle_west(data):
+    something_changed = True
+    while something_changed:
+        something_changed = False
+        for row in range(len(data)):
+            for col in range(1, len(data[0])):
+                if data[row][col] == "O" and data[row][col - 1] == ".":
+                    data[row][col], data[row][col - 1] = data[row][col - 1], data[row][col]
+                    something_changed = True
+
+    return data
+
+
+def cycle_east(data):
+    something_changed = True
+    while something_changed:
+        something_changed = False
+        for row in range(len(data)):
+            for col in range(len(data[0]) - 2, -1, -1):
+                if data[row][col] == "O" and data[row][col + 1] == ".":
+                    data[row][col], data[row][col + 1] = data[row][col + 1], data[row][col]
+                    something_changed = True
+
+    return data
+
+
+def iterate_cycle(data):
+    data = cycle_north(data)
+    data = cycle_west(data)
+    data = cycle_south(data)
+    data = cycle_east(data)
+    return data
+
+
+def p(data):
+    print(np.array(data))
+
+
+def day_14_task_2(data):
+    p(data)
+    n_cycles = 1_000_000_000
+    for i in tqdm(range(n_cycles)):
+        data = iterate_cycle(data)
+    return score(data)
+
+
 if __name__ == '__main__':
     data_data_14_demo = read_data(path="data/day_14_demo.txt")
+    # assert day_14_task_1(data_data_14_demo) == 136
+    # data_data_14 = read_data(path="data/day_14.txt")
+    # assert day_14_task_1(data_data_14) == 109385
 
-    print(np.matrix(data_data_14_demo))
-    print()
-    print(np.matrix(iterate(data_data_14_demo)))
 
-    assert day_14_task_1(data_data_14_demo) == 136
-
-    data_data_14 = read_data(path="data/day_14.txt")
-    print(day_14_task_1(data_data_14))
+    day_14_task_2(data_data_14_demo)
